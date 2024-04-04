07fb8ff50e79e318dba847b9be9758bd6f86cdd0
Adrian Russell <adrianrussell@me.com>
2024-04-04 21:55:55 +0100

    Move GridSubSequence to its own file

---
+++ Sources/Interesting Sequences/Grid/Grid+SubSequence.swift
@@ -0,0 +1,76 @@
+//
+//  Grid+SubSequence.swift
+//
+
+import Foundation
+
+public struct GridSubSequence<Element>: Collection {
+    public typealias Index = Grid<Element>.Index
+    @usableFromInline
+    let base: Grid<Element>
+    @usableFromInline
+    let origin: Point<Int>
+    @usableFromInline
+    let finalIndex: Point<Int>
+    @usableFromInline
+    internal let width: Int
+    @usableFromInline
+    internal let height: Int
+    
+    init(base: Grid<Element>, origin: Point<Int>, width: Int, height: Int) {
+        self.base = base
+        self.origin = origin
+        self.finalIndex = origin + Point(x: width-1, y: height-1)
+        self.width = width
+        self.height = height
+    }
+    @inlinable
+    public var startIndex: Point<Int> { origin }
+    @inlinable
+    public var endIndex: Point<Int> { base.index(after: finalIndex) }
+    @inlinable
+    public subscript(position: Index) -> Element { base[position] }
+    
+    public func index(after: Point<Int>) -> Point<Int> {
+        assert(after < endIndex, "Index out of bounds")
+        guard after < finalIndex else { return endIndex }
+        let x = after.x
+        let y = after.y
+        let maxX = startIndex.x + (width - 1)
+        if x == maxX { return Point(x: startIndex.x, y: y+1) }
+        return Point(x: x+1, y: y)
+    }
+}
+
+extension GridSubSequence: BidirectionalCollection {
+    public func index(before i: Point<Int>) -> Point<Int> {
+        assert(i > startIndex, "Index out of bounds")
+        if i == endIndex { return finalIndex }
+        let x = i.x
+        let y = i.y
+        if x == startIndex.x { return Point(x: x, y: y-1) }
+        return Point(x: x-1, y: y)
+    }
+}
+
+extension GridSubSequence: RandomAccessCollection {}
+
+extension Grid {
+    public func subgrid(origin: Index, width: Int, height: Int) -> GridSubSequence<Element> {
+        assert(origin.x + width <= self.width, "Width too high")
+        assert(origin.y + height <= self.height, "Height too high")
+        return GridSubSequence(base: self, origin: origin, width: width, height: height)
+    }
+}
+
+
+extension Grid {
+    @inlinable
+    public func content(column: Int) -> GridSubSequence<Element> {
+        subgrid(origin: .init(x: column, y: 0), width: 1, height: height)
+    }
+    @inlinable
+    public func content(row: Int) -> GridSubSequence<Element> {
+        subgrid(origin: .init(x: 0, y: row), width: width, height: 1)
+    }
+}
--- Sources/Interesting Sequences/Grid/Grid.swift
+++ Sources/Interesting Sequences/Grid/Grid.swift
@@ -18,6 +18,14 @@ public struct Grid<T> {
         self.height = elements.count
     }
     
+    public init<S: Sequence<T>>(_ elements: S, width: Int, height: Int) {
+        let allElements = Array(elements)
+        assert(allElements.count == width * height, "Invalid element count for width/height")
+        self.elements = allElements
+        self.width = width
+        self.height = height
+    }
+    
     public init(width: Int, height: Int, initial: T) {
         self.elements = Array(repeating: initial, count: width * height)
         self.width = width
@@ -79,85 +87,6 @@ extension Grid: BidirectionalCollection {
 }
 
 extension Grid: RandomAccessCollection { }
-
-
-public struct GridSubSequence<Element>: Collection {
-    @usableFromInline
-    let base: Grid<Element>
-    @usableFromInline
-    let origin: Point<Int>
-    @usableFromInline
-    let finalIndex: Point<Int>
-    
-    private let width: Int
-    private let height: Int
-    
-    init(base: Grid<Element>, origin: Point<Int>, width: Int, height: Int) {
-        self.base = base
-        self.origin = origin
-        self.finalIndex = origin + Point(x: width-1, y: height-1)
-        self.width = width
-        self.height = height
-    }
-    @inlinable
-    public var startIndex: Point<Int> { origin }
-    @inlinable
-    public var endIndex: Point<Int> { base.index(after: finalIndex) }
-    
-    public subscript(position: Point<Int>) -> Element { base[position] }
-    
-    public func index(after: Point<Int>) -> Point<Int> {
-        assert(after < endIndex, "Index out of bounds")
-        guard after < finalIndex else { return endIndex }
-        let x = after.x
-        let y = after.y
-        let maxX = startIndex.x + (width - 1)
-        if x == maxX { return Point(x: startIndex.x, y: y+1) }
-        return Point(x: x+1, y: y)
-    }
-}
-
-extension GridSubSequence: BidirectionalCollection {
-    public func index(before i: Point<Int>) -> Point<Int> {
-        assert(i > startIndex, "Index out of bounds")
-        if i == endIndex { return finalIndex }
-        let x = i.x
-        let y = i.y
-        if x == startIndex.x { return Point(x: x, y: y-1) }
-        return Point(x: x-1, y: y)
-    }
-}
-
-
-extension Grid {
-    public func subgrid(origin: Index, width: Int, height: Int) -> GridSubSequence<Element> {
-        assert(origin.x + width <= self.width, "Width too high")
-        assert(origin.y + height <= self.height, "Height too high")
-        return GridSubSequence(base: self, origin: origin, width: width, height: height)
-    }
-}
-
-
-extension Grid {
-    public func content(column: Int) -> GridSubSequence<Element> {
-        subgrid(origin: .init(x: column, y: 0), width: 1, height: height)
-    }
-    
-    public func content(row: Int) -> GridSubSequence<Element> {
-        subgrid(origin: .init(x: 0, y: row), width: width, height: 1)
-    }
-//    
-//    mutating func insert(columnValue: T, at index: Int) {
-//        for rowIndex in 0..<height {
-//            elements[rowIndex].insert(columnValue, at: index)
-//        }
-//    }
-//    
-//    mutating func insert(rowValue: T, at index: Int) {
-//        let row = Array(repeating: rowValue, count: width)
-//        elements.insert(row, at: index)
-//    }
-}
 
 
 //
