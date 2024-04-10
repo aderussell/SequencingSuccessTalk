28df8072a7167b4cc2c988ab62acbbf820f5812d
Adrian Russell <adrianrussell@me.com>
2024-04-10 21:42:15 +0100

    Update AsyncUnfoldSequenceTests.swift

--- Tests/AsyncUnfoldSequenceTests.swift
+++ Tests/AsyncUnfoldSequenceTests.swift
@@ -13,13 +13,13 @@ fileprivate func throwingAdd(_ v: Int) throws -> Int { v + 1 }
 final class AsyncUnfoldSequenceTests: XCTestCase {
 
     func testExample() async {
-        for await element in asyncSequence(first: 0, next: { $0 + 1 }) {
+        for await element in asyncSequence(first: 0, next: { $0 + 1 }).prefix(10) {
             print(element)
         }
     }
     
     func testExample_throwing() async throws {
-        for try await element in asyncSequence(first: 0, next: { try throwingAdd($0) }) {
+        for try await element in asyncSequence(first: 0, next: { try throwingAdd($0) }).prefix(10) {
             print(element)
         }
     }
