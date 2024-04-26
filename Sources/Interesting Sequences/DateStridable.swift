fd925dce4b6e172ecb22b06d25ed326c782f4c05
Adrian Russell <adrianrussell@me.com>
2024-04-26 20:38:01 +0100

    Add strideDate for DateInterval

--- Sources/Interesting Sequences/DateStridable.swift
+++ Sources/Interesting Sequences/DateStridable.swift
@@ -9,6 +9,8 @@ extension DateComponents {
     public static func minutes(_ value: Int) -> Self { .init(minute: value) }
     public static func hours(_ value: Int) -> Self { .init(hour: value) }
     public static func days(_ value: Int) -> Self { .init(day: value) }
+    public static func weeks(_ value: Int) -> Self { .init(weekOfYear: value) }
+    public static func months(_ value: Int) -> Self { .init(month: value) }
 }
 
 public struct DateStrideSequence: Sequence {
@@ -88,5 +90,14 @@ public func strideDate(from: Date, to: Date, by: DateComponents, calendar: Calen
 
 public func strideDate(from: Date, through: Date, by: DateComponents, calendar: Calendar = .current) -> DateStrideThroughSequence {
     DateStrideThroughSequence(_start: from, _end: through, stride: by, calendar: calendar)
+}
+
+
+public func strideDate(for interval: DateInterval, by: DateComponents, calendar: Calendar = .current) -> DateStrideSequence {
+    DateStrideSequence(_start: interval.start, _end: interval.end, stride: by, calendar: calendar)
+}
+
+public func strideDate(through interval: DateInterval, by: DateComponents, calendar: Calendar = .current) -> DateStrideThroughSequence {
+    DateStrideThroughSequence(_start: interval.start, _end: interval.end, stride: by, calendar: calendar)
 }
 
