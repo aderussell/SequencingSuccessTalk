d1661595a62ca5daf1c3087996af17be7700fe4b
Adrian Russell <adrianrussell@me.com>
2024-04-28 00:11:55 +0100

    Remove old code from main

--- Sources/main.swift
+++ Sources/main.swift
@@ -3,84 +3,6 @@
 
 import Foundation
 
-print("Hello, world!")
-
-struct ExampleSequence: Sequence {
-    typealias Element = Character
-    
-    struct Iterator: IteratorProtocol {
-        var called = false
-        mutating func next() -> Character? {
-            guard !called else { return nil }
-            defer { called = true }
-            return "A"
-        }
-    }
-    
-    func makeIterator() -> Iterator {
-        Iterator()
-    }
-}
-
-struct ExampleCollection: Collection {
-    typealias Element = Character
-    typealias Index = Int
-    
-    var startIndex: Int = 0
-    
-    var endIndex: Int = 1
-    
-    
-    subscript(position: Int) -> Character {
-        "A"
-    }
-    
-    func index(after i: Int) -> Int {
-        0
-    }
-}
-
-
-struct ExampleBiDrectionCollection: BidirectionalCollection {
-    
-    typealias Element = Character
-    typealias Index = Int
-    
-    func index(before i: Int) -> Int {
-        0
-    }
-    
-    func index(after i: Int) -> Int {
-        1
-    }
-    
-    subscript(position: Int) -> Character {
-        "A"
-//        _read {
-//            <#code#>
-//        }
-    }
-    
-    var startIndex: Int = 0
-    
-    var endIndex: Int = 1
-}
-
-
-struct ExampleRandomAccessCollection: RandomAccessCollection {
-    var startIndex: Int = 0
-    var endIndex: Int = 1
-    
-    typealias Element = Character
-    typealias Index = Int
-    
-    subscript(position: Int) -> Character {
-        "A"
-    }
-}
-
-
-
 
 let range = ExampleRange(lower: 0, upper: 10)
 for i in range {
@@ -97,36 +19,6 @@ let l2 = l1.map({ String($0) })
 
 print(l2)
 
-
-struct CounterToTen: IteratorProtocol {
-    typealias Element = Int
-    var val = 0
-    mutating func next() -> Self.Element? {
-        guard val < 10 else { return nil }
-        val += 1
-        return val
-    }
-}
-var counter = CounterToTen()
-while let count = counter.next() {
-    print(count)
-}
-
-
-//for i in CounterToTen() {
-//    print(i)
-//}
-
-
-let i = stride(from: 0, to: 10, by: 1)
-print(i)
-
-
-
-
-for i in Fibonacci().prefix(20) {
-    print(i)
-}
 
 runThemeParkExample()
 runFileSystemExample()
