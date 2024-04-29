b1fd24f82de97bb8bf0df3f48b0feb26dcfb141a
Adrian Russell <adrianrussell@me.com>
2024-04-29 21:39:48 +0100

    Add test for dateStride with filter for weedays

--- Tests/DateStrideSequenceTests.swift
+++ Tests/DateStrideSequenceTests.swift
@@ -82,6 +82,23 @@ final class DateStrideSequenceTests: XCTestCase {
                                 1713742182,
                                 1713655782 ])
     }
+    
+    
+    func testStrideDate_weekdays() throws {
+        let calendar = Calendar(identifier: .gregorian)
+        let start = Date(timeIntervalSince1970: 1713655782)
+        let end   = Date(timeIntervalSince1970: 1714174182)
+        let dates = strideDate(from: start, through: end, by: .days(1), calendar: calendar)
+            .lazy
+            .filter { !calendar.isDateInWeekend($0) }
+            .map { $0.timeIntervalSince1970 }
+            .toArray()
+        XCTAssertEqual(dates, [ 1713742182,
+                                1713828582,
+                                1713914982,
+                                1714001382,
+                                1714087782 ])
+    }
 }
 
 
